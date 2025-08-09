467b0a81e4bc743b32718c725b04a37dafd791ce
Samuel.Adetogun <Samuel.Adetogun@ERP-YP24-030>
2025-08-09 15:05:11 +0100

    Value Object Added (Email Validation)

--- src/Domain/TaskManagementSystem.Domain/ValueObjects/Email.cs
+++ src/Domain/TaskManagementSystem.Domain/ValueObjects/Email.cs
@@ -1,6 +1,17 @@
 ﻿namespace TaskManagementSystem.Domain.ValueObjects
 {
+    // Validating Email At The Domain Level
     public class Email
     {
+        public string Value { get; private set; }
+
+        private Email() { } // EF Core
+        public Email(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value) || !value.Contains("@"))
+                throw new ArgumentException($"Invalid Email Address");
+
+            Value = value;
+        }
     }
 }

42c9452c49400419b8cb13c6b0841a87d198d7bc
Samuel.Adetogun <Samuel.Adetogun@ERP-YP24-030>
2025-08-10 08:18:31 +0100

    Adding My Connection String

--- src/Presentation/TaskManagementSystem.API/Program.cs
+++ src/Presentation/TaskManagementSystem.API/Program.cs
@@ -1,10 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagementSystem.Persistence.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<TaskManagementSystemDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 var app = builder.Build();
 
